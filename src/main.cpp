2f8ce2c73af174f34c4ab90c70574030eb05fcbc
danielcanelhas <daniel.canelhas@gmail.com>
2013-11-18 19:23:16 +0100

    Added sources

---
+++ src/main.cpp
@@ -0,0 +1,99 @@
+#include "sdf.h"
+
+int main( int argc, char* argv[] )
+{
+
+//-----------------------------------------------------------------------------    
+// Define geometry of the world
+//-------------------------------------------------------------------------
+    std::vector<sdf::Primitive*> geometry;
+    Eigen::Vector4d geometryParam;
+
+    // Create the room
+    Eigen::Affine3d T = Eigen::Affine3d::Identity();
+    T.translation() = Eigen::Vector3d(0.0, 0.0, -1.0);
+    geometryParam << 4.0, 4.0, 8.0, 0.0; //dimensions in X Y Z
+    geometry.push_back(new sdf::Box( sdf::bool_union, T, geometryParam , true)); //the last flag flips the sign of the box
+
+    // Add a box inside
+    T = Eigen::Affine3d::Identity();
+    geometryParam << 1.0, 2.0, 0.5, 0.0; //dimensions in X Y Z
+    T.translation() = Eigen::Vector3d(-1.0, 1.0, 2.2);
+    T.linear() = (  Eigen::AngleAxisd(-M_PI*30/180, Eigen::Vector3d::UnitY())).toRotationMatrix(); 
+    geometry.push_back(new sdf::Box( sdf::bool_union, T, geometryParam , false));
+        
+    //Add a Sphere
+    T = Eigen::Affine3d::Identity();
+    T.translation() = Eigen::Vector3d(1.0, 1.5, 2.0);
+    geometryParam << 0.5, 0.0, 0.0, 0.0; // Radius
+    geometry.push_back(new sdf::Sphere( sdf::bool_union, T, geometryParam , false));
+    T = Eigen::Affine3d::Identity();
+   
+    //Add another Box
+    geometryParam << 0.6, 0.6, 0.6, 0.0; //dimensions in X Y Z
+    T.translation() = Eigen::Vector3d(0.5, 2-0.6, 0.5);
+    T.linear() = (  Eigen::AngleAxisd(M_PI*10/180, Eigen::Vector3d::UnitY())).toRotationMatrix(); 
+    geometry.push_back(new sdf::Box( sdf::bool_union, T, geometryParam , false));
+    T = Eigen::Affine3d::Identity();
+
+    //Add a torus
+    T.translation() = Eigen::Vector3d(0.5, 0, 2.25);
+    geometryParam << 0.50, 0.1, 0.0, 0.0; // radius of ring, radius of cross-section
+    geometry.push_back(new sdf::Torus( sdf::bool_union, T, geometryParam , false));
+//-----------------------------------------------------------------------------    
+//Define a camera to use as sensor
+//-----------------------------------------------------------------------------    
+    Eigen::Affine3d Tcam = Eigen::Affine3d::Identity();
+
+    float fx = 300;
+    float fy = 300;
+    float cx = 319.5;
+    float cy = 239.5;
+
+    // focalx,focaly,centerx,centery (image size is determined by Render() fcn )
+    Eigen::Vector4f cam_params(fx, fy, cx, cy);
+
+    cv::Mat Depthmap;
+    cv::Mat Normalmap;
+
+//-----------------------------------------------------------------------------    
+//Do some rendering and animation
+//-----------------------------------------------------------------------------    
+    // A simple animation loop
+    for (int i = -150; i < 60; ++i)
+    {
+      //time steps      
+      double t = double(i);
+
+      // move the camera in an elliptic 3D spiral with pure translation (X Y Z)
+      Tcam.translation() = Eigen::Vector3d( 
+        cos(4*M_PI*t/180), 
+        0.6*sin(4*M_PI*t/180), 
+        t*0.01);
+      
+
+      //change the rotation for one of the objects at each iteration
+      T.linear() = ( 
+                    //Eigen::AngleAxisd(i*M_PI/50, Eigen::Vector3d::UnitZ()) *   
+                    //Eigen::AngleAxisd(i*M_PI/50, Eigen::Vector3d::UnitY()) * 
+                    Eigen::AngleAxisd(i*M_PI/50, Eigen::Vector3d::UnitX()) 
+                    ).toRotationMatrix(); //rotation about X 
+      geometry[4]->transformation = T;
+
+      
+      //Render Normal-map from synthetic environment 
+      sdf::RenderNormal(Tcam, 480, 640, cam_params, 60, 20, 0.02, geometry,Normalmap);
+      cv::imshow("Normals", 0.5-Normalmap*0.5);
+      char q = cv::waitKey(2);
+
+      //Render Depth image from synthetic environment     
+      // sdf::RenderDepth(Tcam, 480, 640, cam_params, 60, 20, 0.02, geometry,Depthmap);      
+      // cv::imshow("Depth", Depthmap/8.0);
+      // char q = cv::waitKey(2);
+
+    if(q == 'q' || q  == 27 || q  == 71 ) { exit(0); }
+
+    }
+    cv::waitKey();
+   return 0;
+}
---
+++ src/sdf.cpp
@@ -0,0 +1,207 @@
+#include "sdf.h"
+#include <iostream>
+
+namespace sdf
+{
+  Eigen::Vector3d 
+  makeRay(int row, int column, float depth, float fx, float fy, float cx, float cy)
+  {
+
+    Eigen::Vector3d ret(double(column-cx)*depth/(fx),
+                        double(row-cy)*depth/(fy),
+                        double(depth));
+    return ret;
+  };
+
+  double 
+  SDF(const Eigen::Vector3d &location, std::vector<Primitive*> &primitives)
+  {
+    double d_value = 0.0;
+
+    for(std::vector<Primitive*>::iterator it = primitives.begin(); it != primitives.end(); ++it)
+    {
+      Eigen::Vector3d location_warped = it[0]->transformation.inverse()*location;
+      if( it == primitives.begin() ) d_value = it[0]->signedDistance(location_warped);
+      else
+      {
+        switch(it[0]->operation)
+        {
+          case bool_union :
+            d_value = std::min( it[0]->signedDistance(location_warped), d_value);
+          
+            break;
+          case bool_intersection :
+            d_value = std::max( it[0]->signedDistance(location_warped), d_value);
+            break;
+          
+          case bool_subtraction :
+            d_value = std::max( -it[0]->signedDistance(location_warped), d_value);
+            break;
+        } 
+      }
+    }
+    return d_value;
+  }
+ 
+  Eigen::Vector3d 
+  SDFGradientPosNorm(const Eigen::Vector3d &location,  std::vector<Primitive*> &primitives)
+  {
+    double delta=1e-05;
+    Eigen::Vector3d v = Eigen::Vector3d(
+           fabs(SDF(location + Eigen::Vector3d(delta,0,0), primitives) - SDF(location - Eigen::Vector3d(delta,0,0), primitives)),
+           fabs(SDF(location + Eigen::Vector3d(0,delta,0), primitives) - SDF(location - Eigen::Vector3d(0,delta,0), primitives)),
+           fabs(SDF(location + Eigen::Vector3d(0,0,delta), primitives) - SDF(location - Eigen::Vector3d(0,0,delta), primitives))/(2.0*delta));
+    return v.normalized();
+  } 
+ 
+  Eigen::Vector3d 
+  SDFGradient(const Eigen::Vector3d &location,  std::vector<Primitive*> &primitives)
+  {
+    double delta=1e-05;
+    return Eigen::Vector3d(
+           SDF(location + Eigen::Vector3d(delta,0,0), primitives) - SDF(location - Eigen::Vector3d(delta,0,0), primitives),
+           SDF(location + Eigen::Vector3d(0,delta,0), primitives) - SDF(location - Eigen::Vector3d(0,delta,0), primitives),
+           SDF(location + Eigen::Vector3d(0,0,delta), primitives) - SDF(location - Eigen::Vector3d(0,0,delta), primitives))/(2.0*delta);
+  }
+
+  void RenderNormal(
+    const Eigen::Affine3d &transformation, 
+    const int rows, 
+    const int cols, 
+    Eigen::Vector4f &cam_params, 
+    const int max_steps, 
+    const float max_ray_length, 
+    const float precision, 
+    std::vector<Primitive*> &primitives,
+    cv::Mat &Normals)
+  {
+    
+    cv::Mat Nmap(rows, cols, CV_32FC3);
+    const Eigen::Vector3d camera = transformation * Eigen::Vector3d(0.0,0.0,0.0);
+    const Eigen::Vector3d viewAxis = (transformation * Eigen::Vector3d(0.0,0.0,1.0+1e-12) - camera).normalized(); 
+    
+    //Rendering loop
+   #pragma omp parallel for 
+    for(int u = 0; u < rows; ++u)
+    {
+      for(int v = 0; v < cols; ++v)
+      {
+        bool hit = false;
+
+        Eigen::Vector3d p = transformation*makeRay(u, v, 1.0, cam_params(0), cam_params(1), cam_params(2), cam_params(3)) - camera;
+        p.normalize();
+              
+        double scaling = 0.4;
+        double scaling_prev=0;
+        double D = 1.0;
+        int steps=0;
+        while(steps<max_steps && scaling < max_ray_length && !hit)
+        { 
+
+          double D_prev = D;
+          D = SDF(camera + p*scaling, primitives);
+          if(D < precision)
+          {
+            scaling = scaling_prev + (scaling-scaling_prev)*D_prev/(D_prev - (D - precision));
+
+            hit = true;
+            Eigen::Vector3d normal_vector = SDFGradient(camera + p*scaling,primitives);            
+            normal_vector.normalize();
+
+            Nmap.at<cv::Vec3f>(u,v)[1]=(normal_vector(0));
+            Nmap.at<cv::Vec3f>(u,v)[2]=(normal_vector(1));
+            Nmap.at<cv::Vec3f>(u,v)[0]=(normal_vector(2));
+
+            break;
+          }
+          scaling_prev = scaling;
+          scaling += D;  
+          ++steps;        
+        }//ray
+        if(!hit)     
+        {
+          Nmap.at<cv::Vec3f>(u,v)[0]=std::numeric_limits<float>::quiet_NaN();
+          Nmap.at<cv::Vec3f>(u,v)[1]=std::numeric_limits<float>::quiet_NaN();
+          Nmap.at<cv::Vec3f>(u,v)[2]=std::numeric_limits<float>::quiet_NaN();
+        
+        }//no hit
+      }//col
+    }//row 
+
+    Normals = Nmap;
+    return;
+  }
+
+  void RenderDepth(
+    const Eigen::Affine3d &transformation, 
+    const int rows, 
+    const int cols, 
+    Eigen::Vector4f &cam_params, 
+    const int max_steps, 
+    const float max_ray_length, 
+    const float precision, 
+    std::vector<Primitive*> &primitives, 
+    cv::Mat &Depth)
+  {
+
+  // add noise to depth
+  // std::mt19937_64 generator;
+  // std::normal_distribution<float> distribution(0,RENDER_NOISE_STD);
+
+  cv::Mat Z_img( rows, cols, CV_32FC1);
+
+   Eigen::Vector3d camera = transformation * Eigen::Vector3d(0.0,0.0,0.0);
+   Eigen::Vector3d viewAxis = (transformation * Eigen::Vector3d(0.0,0.0,1.0+1e-12) - camera).normalized();
+
+  //Rendering loop
+  #pragma omp parallel for 
+  for(int u = 0; u < rows; ++u)
+  {
+    for(int v = 0; v < cols; ++v)
+    {
+      bool hit = false;
+      Eigen::Vector3d p = transformation*makeRay(u, v, 1.0, cam_params(0), cam_params(1), cam_params(2), cam_params(3)) - camera;
+
+
+      p.normalize();
+            
+      double scaling = 0.4;
+      double scaling_prev=0;
+      double D = 1.0;
+      int steps=0;
+      while(steps<max_steps && scaling < max_ray_length && !hit)
+      { 
+
+        double D_prev = D;
+        D = sdf::SDF(camera + p*scaling, primitives);
+        if(D < precision)
+        {
+          if(scaling_prev < 0.4) 
+          {
+              Z_img.at<float>(u,v)=std::numeric_limits<float>::quiet_NaN();    
+              break;
+          }
+          scaling = scaling_prev + (scaling-scaling_prev)*D_prev/(D_prev - (D - precision));
+
+          hit = true;
+          float d = scaling*(viewAxis.dot(p));
+          
+          //conversion from range to depth
+          Z_img.at<float>(u,v)=fabs(d);// + scaling*scaling*distribution(generator)); //add noise dependent on the square of the distance
+          
+          break;
+        }
+        scaling_prev = scaling;
+        scaling += D;  
+        ++steps;        
+      }//ray
+      if(!hit)     
+      {
+        Z_img.at<float>(u,v)=std::numeric_limits<float>::quiet_NaN();    
+      }//no hit
+    }//NUM_COLS
+  }//NUM_ROWS
+  Depth = Z_img;
+  return;
+  }
+}//sdf
